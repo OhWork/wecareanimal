2639ca4810cbcb372815b04969bc38a21f7d29d4
Rachaporn <takbasic722@gmail.com>
2018-03-27 22:34:22 +0700

    we care animal

--- index.php
+++ index.php
@@ -1,4 +1,171 @@
+<!DOCTYPE html>
 <html>
-    <head></head>
-    <body></body>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <link rel="stylesheet" href="CSS/bootstrap.css">
+        <link rel="stylesheet" href="CSS/jquery-ui.css">
+        <link rel="stylesheet" href="CSS/main.css">
+		<link rel="stylesheet" href="CSS/scss/_input-group.scss">
+	</head>
+	<body>
+		<div class="warpper">
+			<div class="container">
+				<div class="row">
+					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="height: 850px;">
+						<div class="row">
+							<div class="col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1">
+								<div class="row">
+									<button type="button" class="btn btn-primary col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top: 47px;">LOGO</button>
+									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 sidemenu">
+										<div class="row">
+											<p class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">Submenu</p>
+											<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+												<div class="row">
+													<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2">
+													</div>
+													<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+														<p>- Add</p>
+													</div>
+													<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2">
+													</div>
+												</div>
+											</div>
+											<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+												<div class="row">
+													<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2">
+													</div>
+													<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+														<p>- Fix</p>
+													</div>
+													<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2">
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-xl-11 col-lg-11 col-md-11 col-sm-11 col-11">
+								<div class="row">
+									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 menumain">
+											<button type="button" class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 menupad">ระบบเวชระเบียน</button>
+											<button type="button" class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 menupad">ระบบการตรวจรักษา</button>
+											<button type="button" class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 menupad">ระบบคลังยา สินค้า และบริการ</button>
+											<button type="button" class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 menupad">ระบบสั่งจ่ายยา สินค้า บริการ</button>
+											<button type="button" class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 menupad">ระบบการเงิน</button>
+											<button type="button" class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 menupad">ระบบนัดหมาย</button>
+									</div>
+									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="background-color:#868e96;border-bottom-right-radius : 7px;">
+										<div class="row">
+											<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6" style="border-right:solid 1px #ffffff;padding-bottom:141px;">
+												<div class="row">
+													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 member">
+														<h3>ระบบเวชระเบียน</h3>
+													</div>
+													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+														<div class="row">
+															<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2"></div>
+															<input type="text" class="form-control col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8" placeholder="รหัสเจ้าของ" aria-label="รหัสเจ้าของ">
+															<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2"></div>
+														</div>
+													</div>
+													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+														<div class="row">
+															<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8"></div>
+															<button type="button" class="btn btn-success col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2" style="margin-top:10px;">Submit</button>
+															<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2"></div>
+														</div>
+													</div>
+													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 member">
+														<h3>ลงทะเบียน</h3>
+													</div>
+													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top:8px;">
+														<div class="row">
+															<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2">
+																<p class="membertext">รหัสเจ้าของ :</p>
+															</div>
+															<input type="text" class="form-control col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+															<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2"></div>
+														</div>
+													</div>
+													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top:8px;">
+														<div class="row">
+															<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2">
+																<p class="membertext">ชื่อ :</p>
+															</div>
+															<input type="text" class="form-control col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+															<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2"></div>
+														</div>
+													</div>
+													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top:8px;">
+														<div class="row">
+															<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2">
+																<p class="membertext">นามสกุล :</p>
+															</div>
+															<input type="text" class="form-control col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+															<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2"></div>
+														</div>
+													</div>
+													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top:8px;">
+														<div class="row">
+															<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2">
+																<p class="membertext">ที่อยู่ :</p>
+															</div>
+															<input type="text" class="form-control col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+															<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2"></div>
+														</div>
+													</div>
+													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top:8px;">
+														<div class="row">
+															<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2">
+																<p class="membertext">เบอร์โทร :</p>
+															</div>
+															<input type="text" class="form-control col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+															<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2"></div>
+														</div>
+													</div>
+													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top:8px;">
+														<div class="row">
+															<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2">
+																<p class="membertext">ชื่อสัตว์เลี้ยง :</p>
+															</div>
+															<input type="text" class="form-control col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+															<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2"></div>
+														</div>
+													</div>
+													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+														<div class="row">
+															<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8"></div>
+															<button type="button" class="btn btn-success col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2" style="margin-top:10px;">Submit</button>
+															<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2"></div>
+														</div>
+													</div>
+												</div>
+											</div>
+											<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6">
+												<div class="row">
+													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 member">
+														<h3>ประวัติการรักษา</h3>
+													</div>
+													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+														<div class="row">
+															<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2"></div>
+															<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8 history"></div>
+															<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2"></div>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+	<footer>
+	</footer>
 </html>
---
+++ login.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <link rel="stylesheet" href="CSS/bootstrap.css">
+        <link rel="stylesheet" href="CSS/jquery-ui.css">
+        <link rel="stylesheet" href="CSS/main.css">
+	</head>
+	<body>
+		<div class="warpper">
+			<div class="container" style="margin-top: 150px;">
+				<div class="row">
+					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top: 20px;">
+						<div class="row">
+							<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+							</div>
+							<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4" style="color:#8C9EFF;">
+								<center><h1 class="glow">WE CARE PETS</h1></center>
+							</div>
+							<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+							</div>
+						</div>
+					</div>
+					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top: 20px;">
+						<div class="row">
+							<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+							</div>
+							<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4 loginbg">
+								<div class="row">
+									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top: 20px;color:#fff;">
+										<center><h3>Login</h3></center>
+									</div>
+									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top: 20px;">
+										<div class="row">
+											<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+												<div class="row">
+													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="color:#fff;">
+														<center><p>Username</p></center>
+													</div>
+													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="color:#fff;">
+														<center><p>Password</p></center>
+													</div>
+												</div>
+											</div>
+											<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+												<div class="row">
+													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+														<input type="text" name="LastName" value="" class="col-12" style="border : solid 1px #00008b;border-radius: 5px;">
+													</div>
+													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top: 12px;">
+														<input type="text" name="LastName" value="" class="col-12" style="border : solid 1px #00008b;border-radius: 5px;">
+													</div>
+												</div>
+											</div>	
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+	<footer>
+	</footer>
+</html>
