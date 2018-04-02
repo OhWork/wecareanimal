504ab4cac1cd451083b059e5418168a3d6639877
Rachaporn <takbasic722@gmail.com>
2018-04-02 23:03:21 +0700

    เพิ่ม

---
+++ stock_show.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <link rel="stylesheet" href="CSS/bootstrap.css">
+        <link rel="stylesheet" href="CSS/jquery-ui.css">
+        <link rel="stylesheet" href="CSS/main.css">
+		<link rel="stylesheet" href="CSS/scss/_input-group.scss">
+	</head>
+	<body>
+		<div class="warpper">
+			<div class="container">
+				<div class="row">
+					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="height: 850px;">
+						<div class="row">
+							<div class="col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1">
+								<div class="row">
+									<button type="button" class="btn btn-primary col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top: 47px;">LOGO</button>
+									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 sidemenu">
+										<div class="row">
+											<p class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">Submenu</p>
+											<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+												<div class="row">
+													<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2">
+													</div>
+													<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+														<p>- Add</p>
+													</div>
+													<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2">
+													</div>
+												</div>
+											</div>
+											<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+												<div class="row">
+													<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2">
+													</div>
+													<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+														<p>- Fix</p>
+													</div>
+													<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2">
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-xl-11 col-lg-11 col-md-11 col-sm-11 col-11">
+								<div class="row">
+									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 menumain">
+											<button type="button" class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 menupad">ระบบเวชระเบียน</button>
+											<button type="button" class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 menupad">ระบบการตรวจรักษา</button>
+											<button type="button" class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 menupad">ระบบคลังยา สินค้า และบริการ</button>
+											<button type="button" class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 menupad">ระบบสั่งจ่ายยา สินค้า บริการ</button>
+											<button type="button" class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 menupad">ระบบการเงิน</button>
+											<button type="button" class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 menupad">ระบบนัดหมาย</button>
+									</div>
+									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="background-color:#868e96;border-bottom-right-radius : 7px;">
+										<div class="row">
+											<!-- ตรงนี้ใส่ Gird view ตารางมีดังนี้  1. ชื่อสินค้า 2. จำนวน 3. ราคาที่ซื้อ 4. ราคาที่ขาย 5.วันที่ซื้อ 6. วันที่ขาย 7. แก้ไข -->
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+	<footer>
+	</footer>
+</html>
