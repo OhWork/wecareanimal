4afd463a115c5b17be893a95a31507a13281a89b
choatchaw <choatchaw@gmail.com>
2018-04-11 22:17:52 +0700

    แก้

---
+++ menumain.php
@@ -0,0 +1,9 @@
+<div class="row">
+									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 menumain">
+											<a class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 menupad">ระบบเวชระเบียน</a>
+											<a class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 menupad">ระบบการตรวจรักษา</a>
+											<a class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 menupad">ระบบคลังยา สินค้า และบริการ</a>
+											<a class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 menupad">ระบบสั่งจ่ายยา สินค้า บริการ</a>
+											<a class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 menupad">ระบบการเงิน</a>
+											<a class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 menupad">ระบบนัดหมาย</a>
+									</div>
