e0359191edbcfcd042d9e37072edf49be2b79d2e
choatchaw <choatchaw@gmail.com>
2018-04-11 22:40:49 +0700

    งง

--- menumain.php
+++ menumain.php
@@ -1,16 +1,6 @@
 <div class="row">
-<<<<<<< HEAD
-									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 menumain">
-											<a class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 menupad" href="index.php?url=add_customer.php">ระบบเวชระเบียน</a>
-											<a class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 menupad">ระบบการตรวจรักษา</a>
-											<a class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 menupad">ระบบคลังยา สินค้า และบริการ</a>
-											<a class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 menupad">ระบบสั่งจ่ายยา สินค้า บริการ</a>
-											<a class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 menupad">ระบบการเงิน</a>
-											<a class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 menupad">ระบบนัดหมาย</a>
-									</div>
-=======
 	<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 menumain">
-		<a class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 menupad">ระบบเวชระเบียน</a>
+		<a class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 menupad" href="index.php?url=add_customer.php">ระบบเวชระเบียน</a>
 		<a class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 menupad">ระบบการตรวจรักษา</a>
 		<a class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 menupad">ระบบคลังยา สินค้า และบริการ</a>
 		<a class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 menupad">ระบบสั่งจ่ายยา สินค้า บริการ</a>
@@ -18,4 +8,3 @@
 		<a class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 menupad">ระบบนัดหมาย</a>
 	</div>
 </div>
->>>>>>> 57f93a2fc5a4bd211414ead50a7cb8c4efd6594a
