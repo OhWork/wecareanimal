a3b4f3160eedb57b3e9953464abe99a95de14f3a
Firore <takbasic722@gmail.com>
2018-04-23 22:05:12 +0700

    อะเอาไปใช้ก่อน

--- add_customer.php
+++ add_customer.php
@@ -8,47 +8,88 @@
     $lbprovince = new label('จังหวัด');
     $lbtell = new label('เบอร์โทรศัพท์');
     $lbline = new label('LineID');
-    
     $txtname = new textfield('-','','form-control','','');
     $txtlastname = new textfield('-','','form-control','','');
-    $txtaddress = new textArea('','','','','');
-    
-    
-    $button = new buttonok("ยืนยัน","","btn btn-success col-md-12","");
-    echo "<h1>ลงทะเบียน</h1>";
-    echo $form->open("","","","insert_customer.php","");
-    echo $lbname;
-    echo $txtname;
-    echo $lblastname;
-    echo $txtlastname;
-    echo $lbaddress;
-    echo $txtaddress;
-    echo $lbsubdistrict;
-    ?>
-     <select id="selSubdistrict">
-    	<option value=""> ------- เลือก ------ </option>
-    </select><span id="waitSubdistrict"></span>
-    <?php
-    echo $lbdistrict;
-    ?>
-    <select id="selDistrict">
-    	<option value=""> ------- เลือก ------ </option>
-    </select><span id="waitDistrict"></span>
-    <?php
-    echo $lbprovince;
-    ?>
-     <select id="selProvince">
-    	<option value=""> ------- เลือก ------ </option>
-        <?php
-            $rs = $db->conditions2("PROVINCE_ID,PROVINCE_NAME","province","CONVERT(province_NAME USING TIS620) ASC")->execute();
-            //$rs = $db->conditions3("district_ID,district_NAME","district","PROVINCE_ID = '1'","CONVERT(province_NAME USING TIS620) ASC")->execute();
-			while($row = mysqli_fetch_assoc($rs)){
-				echo '<option value="', $row['PROVINCE_ID'], '">', $row['PROVINCE_NAME'],'</option>';
-			}
-		?>
-    </select>
-    <?php
-    echo $lbtell;
-    echo $lbline;
-    echo $button;
+    $txtaddress = new textArea('','','','','');$button = new buttonok("ยืนยัน","","btn btn-success col-md-12","");
+	echo $form->open("","","","insert_customer.php","");
 ?>
+<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top:10px;">
+	<div class="row">
+		<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4"></div>
+		<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+			<div class="row">
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+					<h1>ลงทะเบียน</h1>
+				</div>
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top:5px;">
+					<div class="row">
+						<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3" style="padding-top:6px;"><?php echo $lbname; ?></div>
+						<div class="col-xl-9 col-lg-9 col-md-9 col-sm-9 col-9"><?php echo $txtname; ?></div>
+					</div>
+				</div>
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top:5px;">
+					<div class="row">
+						<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3" style="padding-top:6px;"><?php echo $lblastname; ?></div>
+						<div class="col-xl-9 col-lg-9 col-md-9 col-sm-9 col-9"><?php echo $txtlastname; ?></div>
+					</div>
+				</div>
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top:5px;">
+					<div class="row">
+						<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3" style="padding-top:6px;"><?php echo $lbsubdistrict; ?></div>
+						<div class="col-xl-9 col-lg-9 col-md-9 col-sm-9 col-9"><select class="form-control col-12" id="selSubdistrict">
+																				<option value=""> ------- เลือก ------ </option>
+																				</select><span id="waitSubdistrict">
+																				</span>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top:5px;">
+					<div class="row">
+						<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3" style="padding-top:6px;"><?php echo $lbdistrict; ?></div>
+						<div class="col-xl-9 col-lg-9 col-md-9 col-sm-9 col-9"><select class="form-control col-12" id="selDistrict">
+																				<option value=""> ------- เลือก ------ </option>
+																				</select><span id="waitDistrict"></span>
+																				</span>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top:5px;">
+					<div class="row">
+						<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3" style="padding-top:6px;"><?php echo $lbprovince; ?></div>
+						<div class="col-xl-9 col-lg-9 col-md-9 col-sm-9 col-9"><select class="form-control col-12" id="selProvince">
+																				<option value=""> ------- เลือก ------ </option>
+																				<?php
+																					$rs = $db->conditions2("PROVINCE_ID,PROVINCE_NAME","province","CONVERT(province_NAME USING TIS620) ASC")->execute();
+																					//$rs = $db->conditions3("district_ID,district_NAME","district","PROVINCE_ID = '1'","CONVERT(province_NAME USING TIS620) ASC")->execute();
+																					while($row = mysqli_fetch_assoc($rs)){
+																						echo '<option value="', $row['PROVINCE_ID'], '">', $row['PROVINCE_NAME'],'</option>';
+																					}
+																				?>
+																				</select>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top:5px;">
+					<div class="row">
+						<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3" style="padding-top:6px;"><?php echo $lbtell; ?></div>
+						<div class="col-xl-9 col-lg-9 col-md-9 col-sm-9 col-9"></div>
+					</div>
+				</div>
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top:5px;">
+					<div class="row">
+						<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3" style="padding-top:6px;"><?php echo $lbline; ?></div>
+						<div class="col-xl-9 col-lg-9 col-md-9 col-sm-9 col-9"></div>
+					</div>
+				</div>
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top:5px;">
+					<div class="row">
+						<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4"></div>
+						<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4"><?php echo $button; ?></div>
+						<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4"><div>
+	</div>
+</div>
