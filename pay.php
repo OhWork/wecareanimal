3c98d4500fb291dfef7685fb1933502211048a83
beerjrhaa <puttikun@hotmail.com>
2018-04-30 22:01:32 +0700

    หน้าจ่ายตัง

---
+++ pay.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <link rel="stylesheet" href="CSS/bootstrap.css">
+        <link rel="stylesheet" href="CSS/jquery-ui.css">
+        <link rel="stylesheet" href="CSS/main.css">
+		<link rel="stylesheet" href="CSS/scss/_input-group.scss">
+	</head>
+	<body>
+		<div class="warpper">
+			<div class="container">
+				<div class="row">
+					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="height: 850px;">
+						<div class="row">
+							<div class="col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1">
+								<div class="row">
+									<button type="button" class="btn btn-primary col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top: 47px;">LOGO</button>
+									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 sidemenu">
+										<div class="row">
+											<p class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">Submenu</p>
+											<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+												<div class="row">
+													<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2">
+													</div>
+													<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+														<p>- Add</p>
+													</div>
+													<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2">
+													</div>
+												</div>
+											</div>
+											<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+												<div class="row">
+													<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2">
+													</div>
+													<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+														<p>- Fix</p>
+													</div>
+													<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2">
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-xl-11 col-lg-11 col-md-11 col-sm-11 col-11">
+								<div class="row">
+									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 menumain">
+											<button type="button" class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 menupad">ระบบเวชระเบียน</button>
+											<button type="button" class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 menupad">ระบบการตรวจรักษา</button>
+											<button type="button" class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 menupad">ระบบคลังยา สินค้า และบริการ</button>
+											<button type="button" class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 menupad">ระบบสั่งจ่ายยา สินค้า บริการ</button>
+											<button type="button" class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 menupad">ระบบการเงิน</button>
+											<button type="button" class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 menupad">ระบบนัดหมาย</button>
+									</div>
+									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="background-color:#868e96;border-bottom-right-radius : 7px;">
+										<div class="row">
+											<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6">
+												<div class="row">
+													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+														<p>เลือกสินยาและยาที่จำหน่าย</p>
+													</div>
+													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+														<div class="row">
+															<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+																<input class="form-control" type="text" value="เลือกสินค้าหรือยา"/>
+															</div>
+															<div class="col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1">
+																<p>x</p>
+															</div>
+															<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
+																<input class="form-control" type="text" value="จำนวน"/>
+															</div>
+														</div>
+													</div>
+													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+														<div class="row">
+															<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+																<input class="form-control" type="text" value="เลือกสินค้าหรือยา"/>
+															</div>
+															<div class="col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1">
+																<p>x</p>
+															</div>
+															<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
+																<input class="form-control" type="text" value="จำนวน"/>
+															</div>
+														</div>
+													</div>
+													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+														<div class="row">
+															<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+																<input class="form-control" type="text" value="เลือกสินค้าหรือยา"/>
+															</div>
+															<div class="col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1">
+																<p>x</p>
+															</div>
+															<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
+																<input class="form-control" type="text" value="จำนวน"/>
+															</div>
+														</div>
+													</div>
+													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+														<div class="row">
+															<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+																<input class="form-control" type="text" value="เลือกสินค้าหรือยา"/>
+															</div>
+															<div class="col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1">
+																<p>x</p>
+															</div>
+															<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
+																<input class="form-control" type="text" value="จำนวน"/>
+															</div>
+														</div>
+													</div>
+													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+														<div class="row">
+															<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+																<input class="form-control" type="text" value="เลือกสินค้าหรือยา"/>
+															</div>
+															<div class="col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1">
+																<p>x</p>
+															</div>
+															<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
+																<input class="form-control" type="text" value="จำนวน"/>
+															</div>
+														</div>
+													</div>
+													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+														<div class="row">
+															<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+																<input class="form-control" type="text" value="เลือกสินค้าหรือยา"/>
+															</div>
+															<div class="col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1">
+																<p>x</p>
+															</div>
+															<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
+																<input class="form-control" type="text" value="จำนวน"/>
+															</div>
+														</div>
+													</div>
+													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+														<div class="row">
+															<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+																<input class="form-control" type="text" value="เลือกสินค้าหรือยา"/>
+															</div>
+															<div class="col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1">
+																<p>x</p>
+															</div>
+															<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
+																<input class="form-control" type="text" value="จำนวน"/>
+															</div>
+														</div>
+													</div>
+													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+														<div class="row">
+															<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+																<input class="form-control" type="text" value="เลือกสินค้าหรือยา"/>
+															</div>
+															<div class="col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1">
+																<p>x</p>
+															</div>
+															<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
+																<input class="form-control" type="text" value="จำนวน"/>
+															</div>
+														</div>
+													</div>
+													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+														<div class="row">
+															<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+																<input class="form-control" type="text" value="เลือกสินค้าหรือยา"/>
+															</div>
+															<div class="col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1">
+																<p>x</p>
+															</div>
+															<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
+																<input class="form-control" type="text" value="จำนวน"/>
+															</div>
+														</div>
+													</div>
+													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+														<div class="row">
+															<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+																<input class="form-control" type="text" value="เลือกสินค้าหรือยา"/>
+															</div>
+															<div class="col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1">
+																<p>x</p>
+															</div>
+															<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
+																<input class="form-control" type="text" value="จำนวน"/>
+															</div>
+														</div>
+													</div>
+												</div>
+											</div>
+											<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6">
+												<div class="row">
+													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+														<p>รายการสินค้าที่ต้องจ่าย</p>
+													</div>
+													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+														<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="background-color:#ffffff;height: 350px;">
+														</div>
+													</div>
+													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top:5px;">
+														<div class="row">
+															<div class="col-xl-9 col-lg-9 col-md-9 col-sm-9 col-9" style="text-align:right;padding-top:7px;">
+																<p>ราคารวม / total =</p>
+															</div>
+															<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
+																<input class="form-control" type="text" value="จำนวน"/>
+															</div>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top:5px;">
+										<div class="row">
+											<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6">
+											</div>
+											<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
+												<button type="button" class="btn btn-success col-12">จ่ายละจ้า</button>
+											</div>
+											<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
+												<button type="button" class="btn btn-danger col-12">ไม่จ่ายละ</button>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+	<footer>
+	</footer>
+</html>
