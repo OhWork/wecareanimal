203f955257f3a001948a5928b2e0348ebb5c18d8
Firore <takbasic722@gmail.com>
2018-05-07 22:28:22 +0700

    เพิ่มปุ่ม น่ารักม่ะ

--- add_customer.php
+++ add_customer.php
@@ -18,8 +18,23 @@
 ?>
 <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top:10px;">
 	<div class="row">
-		<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4"></div>
-		<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+		<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
+			<div class="row">
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+					<div class="row">
+						<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4"></div>
+						<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+							<div class="row">
+								<button class="btn btn-light col-12" style="margin-top:15px;"><img src="images/user.png" /></button>
+								<button class="btn btn-light col-12" style="margin-top:5px;"><img src="images/pet.png" width="64" height="64" /></button>
+							</div>
+						</div>
+						<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6">
 			<div class="row">
 				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
 					<h1>ลงทะเบียน</h1>
@@ -99,6 +114,6 @@
 				</div>
 			</div>
 		</div>
-		<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4"><div>
+		<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3"><div>
 	</div>
 </div>
